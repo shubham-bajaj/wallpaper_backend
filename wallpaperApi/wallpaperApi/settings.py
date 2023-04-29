83d91385c3279cd9123b18bf67fd6d6fa0105911
Shubham Bajaj <shubhambajaj204@gmail.com>
2023-04-29 17:55:18 +0530

    changed debug to false and added static root

--- wallpaperApi/wallpaperApi/settings.py
+++ wallpaperApi/wallpaperApi/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g0g4)!yuetpxk3vzjv^e7r3_5-l4bqqamddygm9uxq(66gy6x%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
