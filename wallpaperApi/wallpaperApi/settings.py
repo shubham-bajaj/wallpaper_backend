569a4889394f641fe2a35d4914cf5013856150e7
Shubham Bajaj <shubhambajaj204@gmail.com>
2023-04-29 14:30:06 +0530

    corrected

--- wallpaperApi/wallpaperApi/settings.py
+++ wallpaperApi/wallpaperApi/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [BASE_DIR / "api" / "static"]
+STATICFILES_DIRS = [BASE_DIR / "api" / "static"]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
